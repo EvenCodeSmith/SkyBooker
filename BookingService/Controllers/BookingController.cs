058bf80c223df402ad7c7e6de783e6fc93f3d45c
EvenCodeSmith <149378127+EvenCodeSmith@users.noreply.github.com>
2025-06-05 11:26:17 +0200

    Added Booking Service.

---
+++ BookingService/Controllers/BookingController.cs
@@ -0,0 +1,37 @@
+﻿using BookingService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingController : ControllerBase
+    {
+        private readonly BookingDbContext _context;
+
+        public BookingController(BookingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
+            => await _context.Bookings.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Booking>> GetBooking(int id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+            return booking == null ? NotFound() : booking;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Booking>> PostBooking(Booking booking)
+        {
+            _context.Bookings.Add(booking);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBooking), new { id = booking.Id }, booking);
+        }
+    }
+}
---
+++ BookingService/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingService.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FlightId { get; set; } = string.Empty;
+        public string PassengerId { get; set; } = string.Empty;
+        public string PassengerFirstname { get; set; } = string.Empty;
+        public string PassengerLastname { get; set; } = string.Empty;
+        public int TicketCount { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
